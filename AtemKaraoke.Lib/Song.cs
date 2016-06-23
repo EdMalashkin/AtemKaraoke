0ee311ae16594e358240c391c7e0cfbbe27b553b
edmal_000 <edmal_000@NB-EDM>
2016-06-23 20:09:29 +0300

    Selection plus SetSongToPlayer - draft, cannot build

--- AtemKaraoke.Lib/Song.cs
+++ AtemKaraoke.Lib/Song.cs
@@ -1,15 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using AtemKaraoke.Lib.Tools;
+using System;
 
 namespace AtemKaraoke.Lib
 {
 	public class Song : Verse
 	{
-        public Song()
-        {
-
-        }
+        public Song() {  }
 
         public Song(string FilePath) 
 		{
@@ -48,13 +46,17 @@ namespace AtemKaraoke.Lib
                 {
                     _Verses = new List<Verse>();
                     string[] verses = Regex.Split(Text, Config.Default.Splitter);
+                    int accumulatedLength = 0;
                     for (int i = 0; i < verses.Length; i++)
                     {
                         Verse v = new Verse();
-                        v.Text = verses[i].Trim();
+
+                        v.Text = verses[i];
                         v.Number = i + 1;
+                        v.StartPosition = Text.IndexOf(v.Text, accumulatedLength);
 
                         _Verses.Add(v);
+                        accumulatedLength += v.Text.Length;
                     }
                 }
 				return _Verses;
@@ -64,17 +66,68 @@ namespace AtemKaraoke.Lib
 				_Verses = value;
 			}
 		}
-	}
+
+        
+
+        public int SelectVerse(int curPosition)
+        {
+            string splitter = Config.Default.Splitter;
+            int resultNumber = -1;
+            int selectionStart = Text.LastIndexOf(splitter, curPosition);
+            int selectionLength = Text.IndexOf(splitter, curPosition);
+
+            if (curPosition < Text.Length)
+            {
+                int startIndex = curPosition - splitter.Length / 2;
+                if (startIndex < 0) return -1;
+                string curTextToCheckIfSplitter = Text.Substring(startIndex, splitter.Length);
+                if (splitter == curTextToCheckIfSplitter) return -1;
+            }
+
+            if (selectionStart == -1)
+                selectionStart = 0;
+            if (selectionLength == -1)
+                selectionLength = Text.Length;
+
+            selectionLength = selectionLength - selectionStart;
+
+            foreach (Verse v in Verses)
+            {
+                if (v.StartPosition == selectionStart)
+                {
+                    VerseSelectedEventArgs e = new VerseSelectedEventArgs();
+                    e.SelectionNumber = v.Number;
+                    e.SelectionStart = v.StartPosition;
+                    e.SelectionLength = v.EndPosition;
+                    resultNumber = v.Number;
+                    OnVerseSelected(this, e);
+                    break;
+                }
+            }
+
+            return resultNumber;
+        }
+
+        public event VerseSelectedEventHandler VerseSelected;
+        private void OnVerseSelected(object sender, VerseSelectedEventArgs e)
+        {
+            VerseSelected(this, e);
+        }
+    }
+
+    public delegate void VerseSelectedEventHandler(object source, VerseSelectedEventArgs args);
+    public class VerseSelectedEventArgs : EventArgs
+    {
+        public int SelectionStart { get; set; }
+        public int SelectionLength { get; set; }
+        public int SelectionNumber { get; set; }
+    }
 
 
 
-
-
-
-
-	public class Verse
+    public class Verse
 	{
-		protected string _Text;
+		private string _Text;
 		public string Text
 		{
 			get
@@ -83,7 +136,7 @@ namespace AtemKaraoke.Lib
 			}
 			set
 			{
-				_Text = value;
+				_Text = TrimRows(value);
 			}
 		}
 
@@ -113,7 +166,28 @@ namespace AtemKaraoke.Lib
 			}
 		}
 
-		private string _FilePath;
+        private int _startPosition;
+        public int StartPosition
+        {
+            get
+            {
+                return _startPosition;
+            }
+            set
+            {
+                _startPosition = value;
+            }
+        }
+
+        public int EndPosition
+        {
+            get
+            {
+                return StartPosition + Text.Length;
+            }
+        }
+
+        private string _FilePath;
 		public string FilePath
 		{
 			get
@@ -125,5 +199,16 @@ namespace AtemKaraoke.Lib
 				_FilePath = value;
 			}
 		}
-	}
+
+        private string TrimRows(string text)
+        {
+            string[] rows = Regex.Split(text, Environment.NewLine);
+            string newText = "";
+            foreach (string r in rows)
+            {
+                newText += r.Trim() + Environment.NewLine;
+            }
+            return newText.Trim();
+        }
+    }
 }
--- AtemKaraoke.WinForm/Form1.Designer.cs
+++ AtemKaraoke.WinForm/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnGenerate = new System.Windows.Forms.Button();
             this.chkUpload = new System.Windows.Forms.CheckBox();
+            this.chkEditMode = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // txtSong
@@ -40,13 +41,14 @@
             this.txtSong.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtSong.Location = new System.Drawing.Point(12, 12);
+            this.txtSong.Location = new System.Drawing.Point(13, 32);
             this.txtSong.Multiline = true;
             this.txtSong.Name = "txtSong";
             this.txtSong.ReadOnly = true;
-            this.txtSong.Size = new System.Drawing.Size(511, 665);
+            this.txtSong.Size = new System.Drawing.Size(510, 645);
             this.txtSong.TabIndex = 0;
             this.txtSong.Text = resources.GetString("txtSong.Text");
+            this.txtSong.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnMousePressed);
             this.txtSong.MouseUp += new System.Windows.Forms.MouseEventHandler(this.OnMousePressed);
             // 
             // openFileDialog1
@@ -55,9 +57,9 @@
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(529, 49);
+            this.btnGenerate.Location = new System.Drawing.Point(529, 71);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(156, 34);
+            this.btnGenerate.Size = new System.Drawing.Size(146, 34);
             this.btnGenerate.TabIndex = 1;
             this.btnGenerate.Text = "Generate PNG files";
             this.btnGenerate.UseVisualStyleBackColor = true;
@@ -66,18 +68,32 @@
             // chkUpload
             // 
             this.chkUpload.AutoSize = true;
-            this.chkUpload.Location = new System.Drawing.Point(530, 22);
+            this.chkUpload.Location = new System.Drawing.Point(529, 44);
             this.chkUpload.Name = "chkUpload";
             this.chkUpload.Size = new System.Drawing.Size(133, 21);
             this.chkUpload.TabIndex = 2;
             this.chkUpload.Text = "Upload to ATEM";
             this.chkUpload.UseVisualStyleBackColor = true;
             // 
+            // chkEditMode
+            // 
+            this.chkEditMode.AutoSize = true;
+            this.chkEditMode.Checked = true;
+            this.chkEditMode.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkEditMode.Location = new System.Drawing.Point(12, 5);
+            this.chkEditMode.Name = "chkEditMode";
+            this.chkEditMode.Size = new System.Drawing.Size(93, 21);
+            this.chkEditMode.TabIndex = 3;
+            this.chkEditMode.Text = "Edit Mode";
+            this.chkEditMode.UseVisualStyleBackColor = true;
+            this.chkEditMode.CheckedChanged += new System.EventHandler(this.chkEditMode_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(688, 689);
+            this.ClientSize = new System.Drawing.Size(687, 689);
+            this.Controls.Add(this.chkEditMode);
             this.Controls.Add(this.chkUpload);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.txtSong);
@@ -94,6 +110,7 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnGenerate;
         private System.Windows.Forms.CheckBox chkUpload;
+        private System.Windows.Forms.CheckBox chkEditMode;
     }
 }
 
